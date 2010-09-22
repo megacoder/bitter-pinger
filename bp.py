b9a0ce2833dfeb16dedcf873714a8e8576e3ea5d
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2010-09-21 19:39:08 -0500

    Ready to add XOSD interfaces

--- bp.py
+++ bp.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python
 
 import  os
-from    urllib2 import  *
+import  signal
+import  socket
 
-host = 'http://mx.darkzone.un/index.html'
-host = 'http://nonesuch.host.here/index.html'
-host = 'http://localhost/index.html'
-host = 'http://192.168.0.13/index.html'
+host = 'mx.darkzone.un'
+host = 'nonesuch.host.here'
+host = 'localhost'
+host = '192.168.0.13'
+port = 80
 
 class   Bp( object ):
-    def __init__( self, url = host ):
-        self.url           = url
-        self.currently_up  = False
-        self.up_interval   = 60
-        self.down_interval = 30
-        self.seconds       = self.down_interval
+    def __init__( self, host = 'localhost' ):
+        self.host           = host
+        self.currently_up   = False
+        self.previously_up  = False
+        self.up_interval    = 60
+        self.down_interval  = 30
+        self.seconds        = self.down_interval
+        self.connectTimeout = self.down_interval - 1
         return
+
+    def handler( self, signum, frame ):
+        raise IOError, 'Host %d not responding.' % self.host
 
     def poll( self ):
         try:
-            u = urlopen( self.url, timeout=20 )
-            print u.info()
-            u.close()
-            self.currently_up = True
-            self.seconds = self.up_interval
-        except (HTTPError, URLError), e:
-            print '**** BOGUS ****'
-            self.currently_up = False
-            self.seconds = self.down_interval
+            self.sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+            signal.signal( signal.SIGALRM, self.handler )
+            signal.alarm( self.connectTimeout )
+            try:
+                self.sock.connect( self.host, self.port )
+                signal.alarm( 0 )
+                self.sock.close()
+                self.currently_up = True
+                self.seconds = self.up_interval
+            except IOError, e:
+                self.currently_up = False
+                self.seconds      = self.down_interval
+            if( not self.currently_up and self.previously_up ):
+                self.osd.display(
+                    'HOST %s IS NOT RESPONDING!' % self.host
+                )
 
 if __name__ == '__main__':
     clinger = Bp( host )
